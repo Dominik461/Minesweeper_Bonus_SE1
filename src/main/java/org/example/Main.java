06d931b5e4d960619a5eb0374cb702e34f5ae295
Lela <lg100@hdm-stuttgart.de>
2023-01-10 19:09:55 +0100

    Sysout Feld

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,25 +18,30 @@ public class Main {
      *
      * @param args Yet unused
      */
+
     public static void main( String[] args ) {
-
-       // The following statement requires setting
-       // <maven.compiler.source>15</maven.compiler.source>
-       // and <maven.compiler.target>15</maven.compiler.target>
-       // in your project's pom.xml file. In IntelliJ Idea
-       // this may require closing and re-opening your project
-
-//       System.out.println("""
-//         Hi there, let's have
-//         fun learning Java!""");
-
-
-        // Failsafe
-       System.out.println("Hi there,\n let's have\n         fun learning Java!");
-
-        
        log.debug("You may configure 'src/main/resources/log4j2.xml' ");
        log.debug("for adapting both console and 'A1.log' file output");
+
+
+        // X: covered
+        // space = uncovered
+        // o = bomb
+
+
+        System.out.println("    0  1  2  3  4  5  6  7  8  9 ");
+        System.out.println("------------------------------");
+        System.out.println("0 |" );
+        System.out.println("1 |");
+        System.out.println("2 |");
+        System.out.println("3 |");
+        System.out.println("4 |");
+        System.out.println("5 |");
+        System.out.println("6 |");
+        System.out.println("7 |");
+        System.out.println("8 |");
+        System.out.println("9 |");
+
     }
 
     /**
