430eba7f91225c6b4e147c4373b3595a500c544d
Dominik Müller <neptunherbst2000@gmail.com>
2023-01-10 18:44:16 +0100

    Added int size and mines, aswell the array for the fields

--- src/main/java/org/example/GameManager.java
+++ src/main/java/org/example/GameManager.java
@@ -1,35 +1,10 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Random;
 
-/**
- * <p>The sole purpose of this class is about demonstrating
- * <a target="_blank" href="https://www.latex-project.org">LaTeX</a> math formular support in Javadoc.</p>
- *
- */
 public class GameManager {
-
-    /**
-     * <p>Computing the average of a given array's values</p>
-     *
-     * <p>Consider a series of values:</p>
-     *
-     * \[ \textbf{v} = \{v_1, v_2, \dots v_n\} \]
-     *
-     * <p>Its average is being defined as:</p>
-     *
-     * \[ \overline{\textbf{v}} = {1\over n}\sum_{i=1}^n {v_i} \]
-     *
-     * @param values A non-empty array of values.
-     * @return The average of all array values.
-     */
-    public static double average(double[] values) {
-
-        double result = 0;
-        for (final double v: values) {
-            result += v;
-        }
-
-        return result / values.length;
-
-    }
+    int size = 10, mines = 15;
+    ArrayList<Field> fieldArrayList = new ArrayList<>();
 }
+
