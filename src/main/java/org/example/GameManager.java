e33d34b3976cad3def3d11bf29374bac4d295b2a
Dominik Müller <neptunherbst2000@gmail.com>
2023-01-11 15:55:50 +0100

    Added methods to fill the array and set mines

--- src/main/java/org/example/GameManager.java
+++ src/main/java/org/example/GameManager.java
@@ -5,6 +5,27 @@ import java.util.Random;
 
 public class GameManager {
     Random random = new Random(System.currentTimeMillis());
+    public Field[][] fillArray(Field[][] fields, int size, int mines){
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                fields[x][y] = new Field();
+            }
+        }
+        return setMines(fields, size, mines);
+    }
 
+    private Field[][] setMines(Field[][] fields, int size, int mines) {
+        int x, y;
+        for (int i = 0; i <= mines; i++) {
+            do {
+                x = random.nextInt(size);
+                y = random.nextInt(size);
+                if(!fields[x][y].getIsMine())
+                    fields[x][y].setMine(true);
+            } while(!fields[x][y].getIsMine());
+            fields[x][y].setMine(true);
+        }
+        return fields;
+    }
 }
 
