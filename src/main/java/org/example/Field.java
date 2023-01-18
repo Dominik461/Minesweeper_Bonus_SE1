ed406c49e0b7905e3a41508c593747ad59c19df5
Lela <lg100@hdm-stuttgart.de>
2023-01-18 22:36:48 +0100

    removed unnecessary comments

--- src/main/java/org/example/Field.java
+++ src/main/java/org/example/Field.java
@@ -1,26 +1,12 @@
 package org.example;
 
 public class Field {
-    public FieldStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(FieldStatus status) {
-        this.status = status;
-    }
-
-    /*private boolean isBomb, isCovered, isFlag;
-        private String type;*/
     private FieldStatus status;
     private int bombsAround;
     /**
      * Sets the values for each variable when creating an object
      */
     public Field (){
-        /*isBomb = false;
-        isCovered = true;
-        isFlag = false;
-        type = "X";*/
         status = FieldStatus.COVERED;
         bombsAround = 0;
     }
@@ -31,13 +17,21 @@ public class Field {
         UNCOVERED(" "),
         BOMB("X");
 
-        private String status;
+        private final String status;
 
         FieldStatus(final String status) {
             this.status = status;
         }
 
         public String toString() { return status;}
+    }
+
+    public FieldStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FieldStatus status) {
+        this.status = status;
     }
 
     /**
@@ -48,6 +42,7 @@ public class Field {
         status = FieldStatus.COVERED;
         if(!covered) setStatus(FieldStatus.UNCOVERED);
     }
+
     /**
      * Sets the boolean value of the Field object
      * @param bomb will be True if it is a bomb. Otherwise, it will be false
@@ -64,47 +59,4 @@ public class Field {
     public int getBombsAround() {
         return bombsAround;
     }
-
-    /**
-     * Sets the boolean value of the Field object
-     * @return will be True if it is covered. Otherwise, it will be False
-     */
-    /*public boolean getIsCovered() {
-        return isCovered;
-    }*/
-    /**
-     * Sets the boolean value of the Field object
-     * @return will be True if it is a bomb. Otherwise, it will be False
-     */
-    /*public boolean getIsBomb() {
-        return isBomb;
-    }*/
-    /**
-     * Gets the String value of the Field object to be represented in the game board
-     * @return the String value of the Field object
-     */
-    /*public String getType() {
-        return type;
-    }*/
-    /**
-     * Sets the String value of the Field object to be represented in the game board
-     * @param type will on of these options: X: covered, space(" ") = uncovered, Q = bomb
-     */
-    /*public void setType(String type) {
-        this.type = type;
-    }*/
-    /**
-     * Gets the boolean value of the Field object
-     * @return will be True if it is a flag. Otherwise, it will be False
-     */
-    /*public boolean getIsFlag() {
-        return isFlag;
-    }*/
-    /**
-     * Sets the boolean value of the Field object
-     * @param flag will be True if it is a flag. Otherwise, it will be False
-     */
-    /*public void setFlag(boolean flag) {
-        isFlag = flag;
-    }*/
 }
